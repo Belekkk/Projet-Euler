0d455ea937a36b1e04c7479fa112b7ce5adab8f7
Axel Bellec <axel.bellec@outlook.fr>
2015-09-29 13:48:29 +0200

    pb4

--- problem4.go
+++ problem4.go
@@ -2,37 +2,37 @@ package main
 
 import "fmt"
 
-func reverse(val int) int {
-        reversed := 0
-        for val > 0 {
-            reversed = 10*reversed + val%10
-            val = val / 10
-        }
-        return reversed
+func inverse(val int) int {
+    reversed := 0
+    for val > 0 {
+        reversed = 10*reversed + val%10
+        val = val / 10
+    }
+    return reversed
 }
 
 func isPalindrome(val int) bool {
-        if val == reverse(val) {
-            return true
-        }
-        return false
+    if val == inverse(val) {
+        return true
+    }
+    return false
 }
 
 func main() {
-        max := 0
-        a := 999
-        for a >= 100 {
-            b := 999
-            for b >= a {
-                if a*b <= max {
-                    break
-                }
-                if isPalindrome(a * b) {
-                     max = a * b
-                }
-                b --
+    max := 0
+    a := 999
+    for a >= 100 {
+        b := 999
+        for b >= a {
+            if a*b <= max {
+                break
             }
-            a --
+            if isPalindrome(a * b) {
+                 max = a * b
+            }
+            b --
         }
-        fmt.Println(max)
+        a --
+    }
+    fmt.Println(max)
 }
